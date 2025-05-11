132d7ab2d2f64a2a7e38f7d1fa9cb48fc95de088
IOliverC <irene.oliver.correa4@gmail.com>
2025-05-11 21:30:23 +0200

    Added: Primeros pasos, print y types

---
+++ 01_basic/01_print.py
@@ -0,0 +1,11 @@
+###
+# 01 - print()
+# El módulo print() es el módulo que nos permite imprimir en consola
+# Sirve para mostrar información en consola y te va a acompañar
+###
+
+print("¡Hola, mundo!") 
+print("Prueba", "de", "print", "con", "varios", "parámetros")
+print("Prueba", "de", "print", "con", "separación", sep=" - ")
+print("Esto se imprime", end=" ")
+print("en una sola línea")
---
+++ 01_basic/02_types.py
@@ -0,0 +1,29 @@
+###
+# 02 - types()
+# Python tiene varios tipos de datos
+# int, float, complex, str, bool, NoneType, list, tuple, dict, range, set...
+###
+
+print("int:")
+# print(type(10))
+# print(type(0))
+# print(type(-10))
+# print(type(123123112391238901273918273))
+# print(123123112391238901273918273)
+# print(123123112391238901273918273  + 1)
+
+
+print("float:")
+# print(type(3.14))
+# print(type(1.0))
+# print(type(1e3))
+
+
+print("complex:")
+# print(type(3 + 4j))
+
+
+print("str:")
+# print(type("Hola"))
+# print(type(' '))  
+# print(type("123"))
